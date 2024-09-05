0b38592b93ea1bf9a1553718e8292626be94a7e2
Hans2374 <minhkhoi23704@gmail.com>
2024-09-05 08:02:39 +0700

    Stack Layout added

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,8 +1,8 @@
-import { Box } from "@mui/material"
+import { Box, Stack, Divider } from "@mui/material"
 
 export const MuiLayout = () => {
     return (
-        <>
+        <Stack sx={{border: '1px solid'}} direction='row' spacing={2} divider={<Divider orientation="vertical" flexItem/>}>
             <Box
                 sx={{
                     backgroundColor: 'primary.main',
@@ -22,6 +22,6 @@ export const MuiLayout = () => {
                 bgcolor='success.light'
                 p={2}
             ></Box>
-        </>
+        </Stack>
     )
 }
